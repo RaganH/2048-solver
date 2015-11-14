bcab1264089ac402eec28e82be3e58591129a368
Harry Ragan <harry.ragan@gmail.com>
2015-11-14 18:01:52 +0000

    Implement and test slide right array transform

--- src/main/scala/ArrayTransforms.scala
+++ src/main/scala/ArrayTransforms.scala
@@ -20,4 +20,23 @@ object ArrayTransforms {
 
     stack.toArray.reverse.map(x => Some(x)).padTo(boardSlice.length, None)
   }
+
+  def slideRight(boardSlice: Array[Option[Int]]): Array[Option[Int]] = {
+
+    var stack = Stack[Int]()
+
+    for (tile <- boardSlice.reverse)
+      tile match {
+        case None =>
+        case Some(x) => {
+
+          if (!stack.isEmpty && stack.top == x)
+            stack = stack.pop.push(x * 2)
+          else
+            stack = stack.push(x)
+        }
+      }
+
+    stack.toArray.reverse.map(x => Some(x)).padTo(boardSlice.length, None).reverse
+  }
 }
--- src/test/scala/ArrayTransformsSpec.scala
+++ src/test/scala/ArrayTransformsSpec.scala
@@ -27,5 +27,32 @@ object ArrayTransformsSpec extends Specification {
       ArrayTransforms.slideLeft(array) must beEqualTo(Array[Option[Int]](Some(2),Some(4),Some(2),Some(4)))
     }
   }
+
+  "slideRight" should {
+
+    "do nothing for array of None" in {
+      val array = Array[Option[Int]](None, None, None, None)
+
+      ArrayTransforms.slideRight(array) must beEqualTo(Array[Option[Int]](None, None, None, None))
+    }
+
+    "merge consecutive twos" in {
+      val array = Array(Some(2),Some(2),None,None)
+
+      ArrayTransforms.slideRight(array) must beEqualTo(Array(None, None,None,Some(4)))
+    }
+
+    "remove gaps but preserve order" in {
+      val array = Array(Some(2),None,Some(4),None)
+
+      ArrayTransforms.slideRight(array) must beEqualTo(Array(None, None,Some(2),Some(4)))
+    }
+
+    "not modify full array with no adjacent numbers" in {
+      val array = Array[Option[Int]](Some(2),Some(4),Some(2),Some(4))
+
+      ArrayTransforms.slideRight(array) must beEqualTo(Array[Option[Int]](Some(2),Some(4),Some(2),Some(4)))
+    }
+  }
 }
 
