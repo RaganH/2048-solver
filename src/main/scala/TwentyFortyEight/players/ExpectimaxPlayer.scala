f9dbea8c4710990de1f7f7cd17a7b40891f425ae
Harry Ragan <harry.ragan@gmail.com>
2016-01-02 19:01:41 +0000

    Implement expectimax.

--- src/main/scala/TwentyFortyEight/players/ExpectimaxPlayer.scala
+++ src/main/scala/TwentyFortyEight/players/ExpectimaxPlayer.scala
@@ -4,23 +4,44 @@ import TwentyFortyEight._
 import TwentyFortyEight.scorers.BoardScorer
 
 class ExpectimaxPlayer(scorer: BoardScorer) extends Player {
+
+  val directions = Array(Left, Up, Right, Down)
+
   override def getMoveDirection(gameBoard: GameBoard): Direction = {
 
-    val directions = Array(Left, Up, Right, Down)
+    player(gameBoard, 2)._1
 
-    val availableDirections = directions.filter(d => gameBoard.canSlide(d))
-
-    val scoredDirections = availableDirections.map(d => (d, averageGameBoardScore(gameBoard.allSlideOutcomes(d))))
-
-    scoredDirections.reduceLeft((l, r) => if (l._2 > r._2) l else r)._1
   }
 
-  def averageGameBoardScore(gameBoards : Seq[Chance[GameBoard]]) : Double = {
+  def machine(gameBoard: GameBoard, slideDirection : Direction, recursionLimit : Int) : Double = {
 
-    val totalScore = gameBoards.map(c => Chance(c.probability, scorer.scoreSingleGameBoard(c.outcome)))
+    val gameBoards = gameBoard.allSlideOutcomes(slideDirection)
+
+    val totalScore = gameBoards.map(c => Chance(c.probability, player(c.outcome, recursionLimit)._2))
       .map(c => c.probability * c.outcome)
       .sum
 
     totalScore / gameBoards.length
+  }
+
+  def player(gameBoard: GameBoard, recursionLimit : Int) : (Direction, Double) = {
+
+    if (recursionLimit > 0)
+    {
+      val availableDirections = directions.filter(d => gameBoard.canSlide(d))
+
+      if (availableDirections.isEmpty) {
+        //game is over
+        (Left, 0)
+      }
+      else {
+        val scoredDirections = availableDirections.map(d => (d, machine(gameBoard, d, recursionLimit-1)))
+
+        scoredDirections.reduceLeft((l, r) => if (l._2 > r._2) l else r)
+      }
+    }
+    else {
+      (Left, scorer.scoreSingleGameBoard(gameBoard))
+    }
   }
 }
